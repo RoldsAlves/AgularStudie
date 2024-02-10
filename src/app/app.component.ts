35a386b189bc703114d39893b79221d3166dbbbf
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2024-02-10 14:41:02 -0300

    conhecendo o angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, CabecalhoComponent]
+    imports: [RouterOutlet, CabecalhoComponent, RodapeComponent]
 })
 export class AppComponent {
   title = 'memoteca';
